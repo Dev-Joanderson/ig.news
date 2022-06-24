788eae7fe21030e5b17d6dcf58ad5e4c610d1ef8
joanderson <trajanojoanderson@gmail.com>
2022-06-24 16:10:34 -0300

    Updating 🚀

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import styles from './styles.module.scss';
 
-export function SubscribeButton() {
+interface SubscribeButtonProp {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProp) {
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
